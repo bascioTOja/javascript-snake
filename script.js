8c184aede9b499412bd6ba0a6aa6df711020c58f
bascio <hjhjg55sajda@interia.pl>
2024-03-09 12:48:39 +0100

    Initial classes

--- script.js
+++ script.js
@@ -1,3 +1,94 @@
+class Game {
+    static frameRate = 125;
+
+    constructor(canvas, ) {
+        this.canvas = canvas;
+        this.context = canvas.getContext('2d');
+        this.board = new Board(21, '#2e3336', '#3f4549');
+    }
+}
+
+class Board {
+    static tileSize = 21;
+
+    constructor(tileColor, secondaryTileColor) {
+        this.tileColors = [tileColor, secondaryTileColor];
+    }
+
+    draw(context, canvas) {
+        const widthTiles = Math.floor(canvas.width / Board.tileSize);
+        const heightTiles = Math.floor(canvas.height / Board.tileSize);
+
+        for (let y = 0; y < heightTiles; y++) {
+            for (let x = 0; x < widthTiles; x++) {
+                context.fillStyle = this.tileColors[(x + y) % 2];
+                context.fillRect(x * Board.tileSize, y * Board.tileSize, Board.tileSize, Board.tileSize);
+            }
+        }
+    }
+}
+
+class Snake {
+    constructor(length, startPosition) {
+        this.body = Array.from({length}, (_, i) => (
+            new SnakeBody(
+                new Vector(
+                    startPosition.x - i * Board.tileSize,
+                    startPosition.y
+                ),
+                i === 0
+            )
+        ));
+    }
+
+    draw(context, canvas) {
+        this.body.forEach(body => body.draw(context, canvas));
+    }
+}
+
+class SnakeBody {
+    static headColor = '#c48d4d';
+    static bodyColor = '#85c44d';
+
+    constructor(position, is_head = false) {
+        this.position = position;
+        this.is_head = is_head;
+    }
+
+    changeToBody() {
+        this.is_head = false;
+    }
+
+    draw(context, canvas) {
+        this.position.drawRect(context, this.is_head ? SnakeBody.headColor : SnakeBody.bodyColor);
+    }
+}
+
+class Apple {
+    static color = '#ab4343';
+
+    constructor(position) {
+        this.position = position;
+    }
+
+    draw(context, canvas) {
+        this.position.drawRect(context, Apple.color);
+    }
+}
+
+class Vector {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    drawRect(context, color) {
+        context.fillStyle = color;
+        context.fillRect(this.x, this.y, Board.tileSize, Board.tileSize);
+    }
+}
+
+
 (function () {
     const canvas = document.getElementById('canvas');
     const context = canvas.getContext('2d');
